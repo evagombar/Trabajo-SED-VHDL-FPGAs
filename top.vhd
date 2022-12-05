f3e17ad1d431408989ffadb271b2e1c76d8bf7be
evagombar <99722831+evagombar@users.noreply.github.com>
2022-12-05 11:44:56 +0100

    Update top.vhd
    Signed-off-by: evagombar <99722831+evagombar@users.noreply.github.com>

--- top.vhd
+++ top.vhd
@@ -14,18 +14,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity top is
     Port ( 
        bfuera: in std_logic_vector(3 downto 0);
-	   bdentro: in std_logic_vector (3 downto 0);
-	   
-	   piso:inout std_logic_vector(3 downto 0);
-	   
-	   presencia: in std_logic; --sensor que detecta la presencia
-	   pabierta_pcerrada: in std_logic_vector(1 downto 0); --sensor detecta puerta abierta 10, o puerta cerrada 01
-       puerta_motor: out std_logic_vector (1 downto 0);
-       
-       motor: out std_logic_vector (1 downto 0);
+       bdentro: in std_logic_vector (3 downto 0);
+	 
+       presencia: in std_logic; --sensor que detecta la presencia
+       pabierta_pcerrada: in std_logic_vector(1 downto 0); --sensor detecta puerta abierta 10, o puerta cerrada 01
+	    
+	    
+       puerta_motor: out std_logic_vector (1 downto 0); --leds
+       motor: out std_logic_vector (1 downto 0);--leds (motor de ascensor)
 
-	   reset:in std_logic;
-	   clk:in std_logic
+       reset:in std_logic;
+       clk:in std_logic --clk general del programa
+	    
     );
 end top;
 
