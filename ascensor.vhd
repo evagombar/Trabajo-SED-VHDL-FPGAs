451e1814650d5507eeb77696d6fc82329e5e71b6
luciaaantolin <118108413+luciaaantolin@users.noreply.github.com>
2022-12-09 15:21:09 +0100

    Update ascensor.vhd
    Signed-off-by: luciaaantolin <118108413+luciaaantolin@users.noreply.github.com>

--- ascensor.vhd
+++ ascensor.vhd
@@ -1,1 +1,16 @@
 
+architecture Behavioral of pisos is
+begin
+    piso_deseado:process(clk)
+    begin
+        if rising_edge(clk) then
+            case bdentro or bfuera is
+                when "0001" => piso<= "001";
+                when "0010" => piso<= "010";
+                when "0100" => piso<= "011";
+                when "1000" => piso<= "100";
+            end case;
+        end if;
+     end process piso_deseado;
+
+end Behavioral;
