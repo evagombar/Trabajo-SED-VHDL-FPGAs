d137c1f211671a720ceee18c82a34d99fba85f67
evagombar <99722831+evagombar@users.noreply.github.com>
2022-12-15 11:08:10 +0100

    Update top.vhd
    Signed-off-by: evagombar <99722831+evagombar@users.noreply.github.com>

--- top.vhd
+++ top.vhd
@@ -1,39 +1,34 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_arith.ALL;
+use ieee.std_logic_unsigned.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity top is
-    Port ( 
-       bfuera: in std_logic_vector(3 downto 0);
-       bdentro: in std_logic_vector (3 downto 0);
+    port ( 
+    	reset_n:in std_logic;
+    	clk:in std_logic --clk general del programa
+        
+      	bfuera: in std_logic_vector(3 downto 0);
+      	bdentro: in std_logic_vector (3 downto 0);
 	 
        presencia: in std_logic; --sensor que detecta la presencia
        pabierta_pcerrada: in std_logic_vector(1 downto 0); --sensor detecta puerta abierta 10, o puerta cerrada 01
-	    
-	    
+	   
        puerta_motor: out std_logic_vector (1 downto 0); --leds
-       motor: out std_logic_vector (1 downto 0);--leds (motor de ascensor)
+       motor: inout std_logic_vector (1 downto 0);--leds (motor de ascensor)
 
-       reset_n:in std_logic;
-       clk:in std_logic --clk general del programa
+       
 	    
     );
 end top;
 
 architecture Structural of top is
+
 --señales que se asignan a los relojes del programa
-	signal frec1:std_logic; --reloj con mayor frecuencia; apertura y cierre de puertas
-	signal frec2:std_logic;
-	signal frec3:std_logic; --reloj con menor frecuencia.
+	signal frec1:std_logic; --reloj con menor frecuencia; apertura y cierre de puertas
+	signal frec2:std_logic;--reloj con mayor frecuencia; maquinas de estados
+	signal frec3:std_logic; --reloj con frecuencia intermedia;avanzar piso
 
 --señales 
 	signal piso:std_logic_vector (2 downto 0); --piso a donde quiero ir
@@ -51,30 +46,48 @@ END COMPONENT;
 
 COMPONENT ascensor
 	PORT(
-		motor: in std_logic_vector(1 downto 0);
-		presencia: in std_logic;
-		pabierta_pcerrada: in std_logic_vector(1 downto 0);
-		bdentro, bfuera: in std_logic_vector(3 downto 0);
-		piso:out std_logic_vector(2 downto 0);
-		motorpuertas: out std_logic_vector(1 downto 0);
-		reset_n,clk,clk1,clk2: in std_logic	
+      motor: in std_logic_vector(1 downto 0);
+      reset_n:in std_logic;
+      bdentro,bfuera: in std_logic_vector(3 downto 0);
+      clk3:in std_logic; --reloj para ir avanzando de piso
+       
+      piso:out std_logic_vector(2 downto 0);--piso al que quiero ir
+      actual:out std_logic_vector(2 downto 0)--piso en el que estoy 
 	);	
 	
 END COMPONENT;
 	
 COMPONENT control_ascensor
 	PORT(
-		clk,reset_n:in std_logic;
-		piso:in std_logic_vector (2 downto 0);
-		actual: out std_logic_vector (2 downto 0);
-		motor: out std_logic_vector (1 downto 0)	
+		clk1:in std_logic; --cierre de puertas
+        clk2:in std_logic; --maquina de estados
+        reset_n: in std_logic;
+        piso:in std_logic_vector(2 downto 0); --piso donde quiero ir (viene de bloque pisos)->binario
+        actual: in std_logic_vector(2 downto 0); --piso donde se encuentra el ascensor->binario
+        presencia:in std_logic;
+        pabierta_pcerrada: in std_logic_vector(1 downto 0); --sensor detecta puerta abierta 10, o puerta cerrada 01
+        
+        motorpuertas: out std_logic_vector(1 downto 0); --10=abriendo puertas, 01=cerrando puertas,00=parada de puertas
+        motor: out std_logic_vector (1 downto 0); --movimiento del ascensor: "10"=subiendo, "01"=bajando, "00"=parada	
+        led:out std_logic_vector (6 downto 0) --display
 	);
 END COMPONENT;
+
+COMPONENT display
+	PORT(
+    	reset_n : in std_logic;
+        clk3 : in std_logic;  
+        piso_actual : in std_logic_vector (2 downto 0);
+        led : out STD_LOGIC_VECTOR (6 downto 0)
+   	);
+END COMPONENT;
+
 	
 	
 begin
+
 	clock1:clockdivider 
-		GENERIC MAP (frecuencia=>150000000 ) --150MHz
+		GENERIC MAP (frecuencia=>100000 ) --100kHz
    		PORT MAP ( 
         		clock=>clk,
         		reset_n=>reset_n,
@@ -82,7 +95,7 @@ begin
         	);
 		
 	clock2:clockdivider 
-		GENERIC MAP (frecuencia=>100000000 ) --100MHz
+		GENERIC MAP (frecuencia=>150000000 ) --150MHz 150000000
    		PORT MAP ( 
         		clock=>clk,
         		reset_n=>reset_n,
@@ -90,34 +103,46 @@ begin
         	);
 		
 	clock3:clockdivider 
-		GENERIC MAP (frecuencia=>100000 ) --100KHz
+		GENERIC MAP (frecuencia=>100000000 ) --100MHz
    		PORT MAP ( 
         		clock=>clk,
         		reset_n=>reset_n,
        			 clk=>frec3
         	);
+            
+    display1:display
+    	PORT MAP(
+          reset_n=>reset_n,
+          clk3=>frec3,  
+          piso_actual=>pisoactual,
+          led=>led
+        
+        );
+        
 	ascensor1:ascensor
 		PORT MAP(
-			motor=> motor,
-			presencia=> presencia,
-			pabierta_pcerrada=> pabierta_pcerrada,
-			bdentro=>bdentro, 
-			bfuera=>bfuera,
-			piso=>piso,
-			motorpuertas=> puerta_motor,
-			reset_n=> reset_n,
-			clk1=>frec1,
-			clk2=>frec2,
-			clk=>frec3
+          motor=>motor,
+          reset_n=>reset_n,
+          bdentro=>bdentro,
+          bfuera=>bfuera,
+          clk3=>frec3,
+
+          piso=>piso,
+          actual=>pisoactual
+			
 		);
 	
 	c_ascensor:control_ascensor
 		PORT MAP(
-			clk=> frec3,
-			reset_n=> reset_n,
-			piso=>piso,
-			actual=>pisoactual,
-			motor=>motor --motor del ascensor
+          clk1=>frec1,
+          clk2=>frec2,
+          reset_n=>rest_n,
+          piso=>piso,
+          actual=>pisoactual,
+          presencia=>presencia,
+          pabierta_pcerrada=>pabierta_pcerrada,
+          motorpuertas=>motorpuertas,
+          motor=>motor
 		);
 		
 end Structural;
