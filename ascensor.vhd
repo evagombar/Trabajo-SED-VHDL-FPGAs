58f8cb5293eb862e9d590f325d1a088185d458ca
martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>
2023-01-03 13:50:12 +0100

    Update ascensor.vhd
    Signed-off-by: martaseisdedos <100596598+martaseisdedos@users.noreply.github.com>

--- ascensor.vhd
+++ ascensor.vhd
@@ -1,101 +1,134 @@
 library IEEE;
+
 use IEEE.STD_LOGIC_1164.ALL;
+
+use ieee.std_logic_arith.ALL;
+
+use ieee.std_logic_unsigned.ALL;
+
+
 
 
 entity ascensor is
-    port(
-	   motor: in std_logic_vector(1 downto 0);
-	   presencia: in std_logic; --sensor que detecta la presencia
-	   pabierta_pcerrada: in std_logic_vector(1 downto 0); --sensor detecta puerta abierta 10, o puerta cerrada 01
-           bdentro,bfuera: in std_logic_vector(3 downto 0); 
-           motorpuertas: out std_logic_vector(1 downto 0); --10=abriendo puertas, 01=cerrando puertas,00=parada de puertas
-	   piso:out std_logic_vector(2 downto 0);--piso al que quiero ir 
 
-	   reset_n:in std_logic;
-	   clk:in std_logic;--reloj para determinar el piso al que quiero ir
-	   clk1:in std_logic; --reloj para pasar de estado
-	   clk2:in std_logic --reloj espera de seguridad de cerrar puertas
+    port(
+
+	   motor: in std_logic_vector(1 downto 0);
+
+       reset_n:in std_logic;
+
+       boton: in std_logic_vector(3 downto 0);
+
+       clk3:in std_logic; --reloj para ir avanzando de piso
+
+        aux: in std_logic;
+
+       piso:out std_logic_vector(2 downto 0);--piso al que quiero ir
+
+       actual:out std_logic_vector(2 downto 0)--piso en el que estoy 
+
 );
+
+       
+
 	
+
 end ascensor;
 
+
+
+
 architecture Behavioral of ascensor is
-   type estados is (PARADA,ABRIENDO,CERRANDO,PAUSASEGURIDAD,ESPERABOTON);
-   signal ESTADO_ACT,ESTADO_SIG: estados;
 
-begin
 
-clock: process(reset_n,clk1)
+
+
+	signal p_act: std_logic_vector (2 downto 0); --asignacion del piso
+
+	
+
 	begin
-		if(reset_n='1') then
-			ESTADO_ACT<=PARADA;
-		elsif (rising_edge(clk1)) then
-			ESTADO_ACT<=ESTADO_SIG;
-		end if;
-end process clock;
 
-maquina: process(ESTADO_ACT,motor,pabierta_pcerrada,presencia,bdentro,bfuera,clk2)
-	begin
-	  case ESTADO_ACT is
-		  when PARADA=>
-        motorpuertas<="00";
-			  if(pabierta_pcerrada="01" and motor="00") then
-				  ESTADO_SIG<=ABRIENDO; --apertura de puertas
-			  else
-				ESTADO_SIG<=PARADA; 
-        end if;
+    piso_deseado:process(clk3,reset_n,aux)
+
+    	begin
+
+       		if(reset_n='0')then
+
+        		piso<="001";
+
+          	elsif (rising_edge(clk3) and motor="00" and aux='1' ) then
+          
+          		case boton is --asigna el piso al que quiere ir
+          		    
+          		  when "0000" => piso<= p_act;
+
+                  when "0001" => piso<= "001";
+
+                  when "0010" => piso<= "010";
+
+                  when "0100" => piso<= "011";
+
+                  when "1000" => piso<= "100";
 
 
-		  when ABRIENDO=>
-        motorpuertas<="10";
-		    if(pabierta_pcerrada="10") then
-				  ESTADO_SIG<=PAUSASEGURIDAD; --ascensor ha llegado a destino
-			  else
-				  ESTADO_SIG<=ABRIENDO; --ascensor sigue en movimiento
-			  end if;
 
-		  when PAUSASEGURIDAD=>
-      motorpuertas<="00";
-			if(presencia='0' and rising_edge(clk2))then --puerta esta abierta, no hay presencia y ha pasado el tiempo de seguridad
-				ESTADO_SIG<=CERRANDO; --se cierran las puertas
-			else
-				ESTADO_SIG<=PAUSASEGURIDAD;
-			end if;
 
-		  when CERRANDO=>
-            motorpuertas<="01";
-			if(presencia='1' and pabierta_pcerrada/="01")then --está cerrando y nota presencia
-				ESTADO_SIG<=ABRIENDO;
-			elsif(pabierta_pcerrada="01") then
-				ESTADO_SIG<=ESPERABOTON;
-			else
-				ESTADO_SIG<=CERRANDO;
-			end if;
+                  when others => piso<= p_act;
+
+           		end case;
+
+          	end if;
+
+          	
+
+    end process piso_deseado;
+
+    
+
+    
+
+    movimiento:process(motor,clk3,reset_n)
+
+    	begin
+
+        	if(reset_n='0')then
+
+            	p_act<="001";
+         
+
+            elsif(rising_edge(clk3)) then
+
+            
+
+            	if(motor="01")then
+
+                	p_act<=p_act-1;
+
+                elsif(motor="10")then
+
+                	p_act<=p_act+1;
+
+                else
+
+                	p_act<=p_act;
+
+                end if;
+
+                
+
+           	end if;
+
+            
+
+    end process movimiento;
+
+    
+
+    actual<=p_act;
+
         
-      when ESPERABOTON=>
-        motorpuertas<="00";
-			if(bdentro/="0000" or bfuera/="0000") then --se pulsa un botón
-				ESTADO_SIG<=PARADA;
-      else
-        ESTADO_SIG<=ESPERABOTON;
-      end if;
-	end case;
-		
- end process maquina;
 
-    piso_deseado:process(clk, motor)
-    begin
-        if rising_edge(clk) then
-		if (motor="00") then --solo hace caso al botón si el  motor del ascensor está parado
-		    case bdentro or bfuera is
-			when "0001" => piso<= "001";
-			when "0010" => piso<= "010";
-			when "0100" => piso<= "011";
-			when "1000" => piso<= "100";
-	         	when others => piso<= "000";
-		    end case;
-		end if;
-        end if;
-     end process piso_deseado;
+  
 
 end Behavioral;
